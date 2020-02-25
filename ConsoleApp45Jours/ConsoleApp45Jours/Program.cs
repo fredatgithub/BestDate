5d4e6ad74994111c354ded1121eaa4f9af7094b5
Fred <domaine75@gmail.com>
2020-02-25 19:54:51 +0100

    Create helper DLL

--- ConsoleApp45Jours/ConsoleApp45Jours/Program.cs
+++ ConsoleApp45Jours/ConsoleApp45Jours/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HelperLibrary;
 
 namespace ConsoleApp45Jours
 {
@@ -13,39 +14,14 @@ namespace ConsoleApp45Jours
       {
         uneDate = new DateTime(2020, 2, i);
         displayOneLine($"{uneDate.ToShortDateString()};");
-        displayOneLine($"{EndOfMonthPlus45Days(uneDate).ToShortDateString()};"); // fin du mois + 45 jours 
-        displayOneLine($"{FourtyFiveDaysplusEndOfTheMonth(uneDate).ToShortDateString()}"); //  45 jours + fin du mois
-        displayOneLine($" Best date is : {BestDate(uneDate).ToShortDateString()}");
+        displayOneLine($"{DateTimeHelper.EndOfMonthPlus45Days(uneDate).ToShortDateString()};"); // fin du mois + 45 jours 
+        displayOneLine($"{DateTimeHelper.FourtyFiveDaysplusEndOfTheMonth(uneDate).ToShortDateString()}"); //  45 jours + fin du mois
+        displayOneLine($" Best date is : {DateTimeHelper.BestDate(uneDate).ToShortDateString()}");
         display("");
       }
 
       display("Press any key to exit:");
       Console.ReadKey();
-    }
-
-    public static DateTime EndOfMonthPlus45Days(DateTime date)
-    {
-      DateTime endOfTheMonth = EndOfMonth(date);
-      return endOfTheMonth.AddDays(45);
-    }
-
-    public static DateTime FourtyFiveDaysplusEndOfTheMonth(DateTime date)
-    {
-      DateTime datePlus45 = date.AddDays(45);
-      DateTime endOfTheMonth = EndOfMonth(datePlus45);
-      return endOfTheMonth;
-    }
-
-    public static DateTime EndOfMonth(DateTime date)
-    {
-      return new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
-    }
-
-    public static DateTime BestDate(DateTime oneDate)
-    {
-      DateTime firstDate = EndOfMonthPlus45Days(oneDate);
-      DateTime secondDate = FourtyFiveDaysplusEndOfTheMonth(oneDate);
-      return firstDate <= secondDate ? firstDate : secondDate;
     }
   }
 }
---
+++ ConsoleApp45Jours/HelperLibrary/DateTimeHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelperLibrary
+{
+  public static class DateTimeHelper
+  {
+    public static DateTime EndOfMonthPlus45Days(DateTime date)
+    {
+      DateTime endOfTheMonth = EndOfMonth(date);
+      return endOfTheMonth.AddDays(45);
+    }
+
+    public static DateTime FourtyFiveDaysplusEndOfTheMonth(DateTime date)
+    {
+      DateTime datePlus45 = date.AddDays(45);
+      DateTime endOfTheMonth = EndOfMonth(datePlus45);
+      return endOfTheMonth;
+    }
+
+    public static DateTime EndOfMonth(DateTime date)
+    {
+      return new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month));
+    }
+
+    public static DateTime BestDate(DateTime oneDate)
+    {
+      DateTime firstDate = EndOfMonthPlus45Days(oneDate);
+      DateTime secondDate = FourtyFiveDaysplusEndOfTheMonth(oneDate);
+      return firstDate <= secondDate ? firstDate : secondDate;
+    }
+  }
+}
